b3cd3323408e794680090b49c8cbf5d1ebcf7084
geraldarzy <geraldarzola@gmail.com>
2021-01-22 23:43:59 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
-
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+    get '/name' do
+        "My name is Arzy"
+    end
+    get '/hometown' do
+        "My hometown is Gerald"
+    end
+    get '/favorite-song' do
+        "My favorite song is Oh Yeah"
+    end
 end
